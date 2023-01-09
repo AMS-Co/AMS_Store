aa0cd78380f3a43b320c7ca5884233be73f3cc6d
Alireza Bagheri <78638285+alirezabagheri4@users.noreply.github.com>
2023-01-09 20:31:06 +0330

    ..

--- Modules/CMS/1-Service/Api/Program.cs
+++ Modules/CMS/1-Service/Api/Program.cs
@@ -1,3 +1,4 @@
+using Api.Builders;
 using Api.Configurations;
 using Application.CustomerService.Command;
 using MediatR;
@@ -6,61 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Configuration
-    .SetBasePath(builder.Environment.ContentRootPath)
-    .AddJsonFile("appsettings.json", true, true)
-    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
-    .AddEnvironmentVariables();
-
-builder.Services.AddControllers();
-
-// Setting DBContexts
-builder.Services.AddDatabaseConfiguration(builder.Configuration);
 
 
-// ASP.NET Identity Settings & JWT
-//builder.Services.AddApiIdentityConfiguration(builder.Configuration);
-
-// Interactive AspNetUser (logged in)
-// NetDevPack.Identity dependency
-//builder.Services.AddAspNetUserConfiguration();
-
-// AutoMapper Settings
-builder.Services.AddAutoMapperConfiguration();
-
-// Swagger Config
-builder.Services.AddSwaggerConfiguration();
-
-// Adding MediatR for Domain Events and Notifications
-builder.Services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
-
-builder.Services.AddDependencyInjectionConfiguration();
-
-var app = builder.Build();
-
-
-// .NET Native DI Abstraction
-
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-
-
-
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.UseSwaggerSetup();
+var app = builder.ConfigureServices().ConfigurePipeLines();
 
 app.Run();
