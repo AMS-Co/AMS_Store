512c72b6a9c4ba5b854a11dc3c626ccc76995041
Alireza Bagheri <78638285+alirezabagheri4@users.noreply.github.com>
2023-01-09 20:31:18 +0330

    ..

--- Modules/CMS/1-Service/Api/Program.cs
+++ Modules/CMS/1-Service/Api/Program.cs
@@ -1,14 +1,5 @@
 using Api.Builders;
-using Api.Configurations;
-using Application.CustomerService.Command;
-using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-
-
 var app = builder.ConfigureServices().ConfigurePipeLines();
-
 app.Run();
